ff9efc8171784a394c4ea842c7d8e8aafdf7dbfe
Huan <753132377@qq.com>
2016-12-20 15:56:22 +0800

    修改了后台主页，添加了上传图片的功能（huan）

---
+++ application/common/validate/Commodity.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 商品验证器.
+ * User: Huan
+ * Date: 2016/12/20
+ * Time: 15:49
+ */
+
+namespace app\common\validate;
+
+
+use think\Validate;
+
+class Commodity extends Validate
+{
+
+
+}
