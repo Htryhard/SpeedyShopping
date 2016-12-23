f3219f56022644474f93236998972a1b0467674d
Huan <753132377@qq.com>
2016-12-24 00:18:29 +0800

    在前端添加了登陆、注册页面（huan）

--- application/home/controller/IndexController.php
+++ application/home/controller/IndexController.php
@@ -2,6 +2,7 @@
 namespace app\home\controller;
 
 use app\common\controller\BaseController;
+use app\common\model\Commodity;
 use think\Request;
 
 class IndexController extends  BaseController
@@ -11,6 +12,29 @@ class IndexController extends  BaseController
         $this->assign('email','你好');
         return $this->fetch();
 
+    }
+
+    public function search(){
+        $title = $this->request->param();
+        // 获取查询信息
+        $title = input('get.title');
+        $pageSize = 30; // 每页显示15条数据
+        $commodity = new Commodity();
+        // 定制查询信息
+        if (!empty($title)) {
+            $commodities = $commodity->where('title', 'like', '%' . $title . '%')->paginate($pageSize, false,
+                [
+                    'query' => [
+                        'title' => $title,
+                    ],
+                ]);
+        } else {
+            $commodities = $commodity->paginate($pageSize);
+        }
+        $imgRoot = '/speedyshopping/public//uploads/commodity_images/';
+        $this->assign("imgRoot",$imgRoot);
+        $this->assign('commodities', $commodities);
+        return $this->fetch();
     }
 
 
