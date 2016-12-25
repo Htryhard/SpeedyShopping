b33024bce0e63bc5b67956846315811f3fadc450
Huan <753132377@qq.com>
2016-12-26 00:43:36 +0800

    前台增加了商品的明细页面（huan）

--- application/home/controller/CommodityController.php
+++ application/home/controller/CommodityController.php
@@ -9,13 +9,15 @@
 namespace app\home\controller;
 
 
+use app\common\Comm;
 use app\common\controller\BaseController;
 use app\common\model\Commodity;
 
 class CommodityController extends BaseController
 {
 
-    public function search(){
+    public function search()
+    {
         $title = $this->request->param();
         // 获取查询信息
         $title = input('get.title');
@@ -33,9 +35,43 @@ class CommodityController extends BaseController
             $commodities = $commodity->paginate($pageSize);
         }
         $imgRoot = '/speedyshopping/public//uploads/commodity_images/';
-        $this->assign("imgRoot",$imgRoot);
+        $this->assign("imgRoot", $imgRoot);
         $this->assign('commodities', $commodities);
         return $this->fetch();
+    }
+
+    /**
+     * 前端商品明细页面
+     * @param string $id
+     * @return mixed|string
+     */
+    public function commodityDetailed($id = "")
+    {
+        if ($id != "") {
+            $commodity = Commodity::get(['id' => $id]);
+            if ($commodity != null) {
+//                $commodityData = Comm::analysisCommodityForm($commodityData);
+                //构造商品的图片
+                $commodityImages = array();
+                $commodityImageObjects = $commodity['images'];
+                foreach ($commodityImageObjects as $imageObject){
+                    $imgUrl = '/speedyshopping/public//uploads/commodity_images/' . $imageObject->getData("image");
+                    array_push($commodityImages,$imgUrl);
+                }
+
+                //构造商品的参数
+                $commodityParameters = Comm::jsonToArr($commodity->getData("parameter"));
+
+                $this->assign("commodity",$commodity);
+                $this->assign("parameters",$commodityParameters);
+                $this->assign("images",$commodityImages);
+                return $this->fetch();
+            } else {
+                return "商品不存在";
+            }
+        } else {
+            return "没有转入指定参数";
+        }
     }
 
 }
