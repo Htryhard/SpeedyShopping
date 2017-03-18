5c6902358a9ebe61ab95879b722cf6024534adf3
Huan <753132377@qq.com>
2017-03-19 02:41:12 +0800

    添加了评论功能

--- application/api/controller/CommodityController.php
+++ application/api/controller/CommodityController.php
@@ -10,16 +10,68 @@ namespace app\api\controller;
 
 
 use app\common\model\Commodity;
+use app\common\model\CommodityImages;
 use think\Controller;
+use think\Request;
 
 class CommodityController extends Controller
 {
-    public function getAllCommodity(){
+    public function getAllCommodity()
+    {
         header("Cache-control:max-age=5");
         $commodities = Commodity::all();
         return json($commodities);
     }
 
-    public function getCommodities(){}
+    /**
+     * 根据关键词进行搜索
+     * @return \think\response\Json
+     */
+    public function getCommodities()
+    {
+        $commodity = new Commodity();
+        $where = Request::instance()->get("where");
+        if ($where != "") {
+            $commodities = $commodity->where('title', 'like', '%' . $where . '%')->where('out_time', '>', time())->select();
+        } else {
+            $commodities = Commodity::all(function ($query) {
+//            $query->where('out_time', '>', time())->order('id', 'desc');
+                $query->where('out_time', '>', time());
+            });
+        }
+        if (count($commodities) > 0) {
+            return json($commodities);
+        } else {
+            return json(['error' => '还没有电影哦~'], 404);
+        }
+
+    }
+
+    /**
+     * 根据商品ID获取商品图片
+     * @return \think\response\Json
+     */
+    public function getCommodityImages()
+    {
+        //商品ID
+        $where = Request::instance()->get("where");
+        if ($where!=""){
+            $commoditiyImages = CommodityImages::all(['commodity_id'=>$where]);
+            return json($commoditiyImages);
+        }else{
+            return json(["error"=>"参数错误！"]);
+        }
+    }
+
+    public function getSpecificationForCommodity(){
+        //商品ID
+        $where = Request::instance()->get("where");
+        if ($where!=""){
+            $specificationes = Commodity::get(['id'=>$where])['specifications'];
+            return json($specificationes);
+        }else{
+            return json(["error"=>"参数错误！"]);
+        }
+    }
 
 }
